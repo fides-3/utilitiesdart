fbd25f1269a545cb42b27033934781923fed58de
ntoiti <fidesluckia36@gmail.com>
2024-04-02 20:49:56 +0300

    first commit

---
+++ utilities.dart
@@ -0,0 +1,33 @@
+//function that returns sum of two numbers
+int sum(int a ,int b){
+return a+b;
+}
+//printing numbers from 1 to 10 using for loop
+//void main(){
+  //for(int i=1;i<=10;i++){
+   // print(i);
+ // }
+
+//}
+//switchcase statement to check for different string values
+// void checkString(String value){
+//   switch (value){
+//     case "hello":
+  //   print("hello there!");
+  //   break;
+  // case "bye":
+  //   print("goodbye!");
+    //break;
+  //default:
+   // print("unknown value");
+
+  //}
+//}
+//printing numbers from 20 to 10 using a while loop
+void main(){
+int i=20;
+while(i>=10){
+  print(i);
+  i--;
+}
+}
